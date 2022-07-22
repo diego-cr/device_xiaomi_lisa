82a522f742fdc398a729001618baab15cb3a105b
diegocr@gmail.com <diegocr@gmail.com>
2022-07-22 10:20:56 +0200

    Aicpify
    Change-Id: I3441584e20f86397ff4bf18e77dc69ab58c60e87

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lisa.mk
+    $(LOCAL_DIR)/aicp_lisa.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lisa-user \
-    lineage_lisa-userdebug \
-    lineage_lisa-eng
+    aicp_lisa-user \
+    aicp_lisa-userdebug \
+    aicp_lisa-eng
